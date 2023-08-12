02ee4e546ff4189c4c38687a59055367772a552c
Paul D'Angio <pcdangio@gmail.com>
2023-08-12 13:39:49 -0400

    test: initial commit of test suite for configuration object

---
+++ test/configuration.cpp
@@ -0,0 +1,22 @@
+// ads101x
+#include <ads101x/configuration.hpp>
+
+// gtest
+#include <gtest/gtest.h>
+
+TEST(configuration, default_constructor)
+{
+    // Create default-constructed configuration.
+    ads101x::configuration config;
+
+    // Verify fields.
+    EXPECT_EQ(config.operation, ads101x::operation::IDLE);
+    EXPECT_EQ(config.multiplexer, ads101x::multiplexer::AIN0_AIN1);
+    EXPECT_EQ(config.fsr, ads101x::fsr::FSR_2_048);
+    EXPECT_EQ(config.mode, ads101x::mode::MODE_SINGLESHOT);
+    EXPECT_EQ(config.data_rate, ads101x::data_rate::SPS_1600);
+    EXPECT_EQ(config.comparator_mode, ads101x::comparator_mode::TRADITIONAL);
+    EXPECT_EQ(config.comparator_polarity, ads101x::comparator_polarity::ACTIVE_LOW);
+    EXPECT_EQ(config.comparator_latch, ads101x::comparator_latch::NONLATCHING);
+    EXPECT_EQ(config.comparator_queue, ads101x::comparator_queue::DISABLED);
+}
