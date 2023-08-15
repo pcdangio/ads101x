503632994f2266e6e10de874a046738dd8ce192f
Paul D'Angio <pcdangio@gmail.com>
2023-08-15 17:03:22 -0400

    address: added slave_address for i2c communication

--- include/ads101x/address.hpp
+++ include/ads101x/address.hpp
@@ -8,8 +8,17 @@
 
 namespace ads101x {
 
+/// \brief An enumeration of I2C slave addresses for the ADS101x family.
+enum class slave_address : uint8_t
+{
+    GND_PIN     = 0b01001000,   ///< Slave address when ADDR_PIN is connected to GND_PIN.
+    VDD_PIN     = 0b01001001,   ///< Slave address when ADDR_PIN is connected to VDD_PIN.
+    SDA_PIN     = 0b01001010,   ///< Slave address when ADDR_PIN is connected to SDA_PIN.
+    SCL_PIN     = 0b01001011    ///< Slave address when ADDR_PIN is connected to SCL_PIN.
+};
+
 /// \brief An enumeration of I2C register addresses for the ADS101x family.
-enum class address : uint8_t
+enum class register_address : uint8_t
 {
     CONVERSION  = 0b00000000,   ///< The conversion register address.
     CONFIG      = 0b00000001,   ///< The configuration register address.
